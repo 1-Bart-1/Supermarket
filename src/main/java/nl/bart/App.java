9c63ed6aa543f048dc3347e70d861bd462afbe4a
1-Bart-1 <bart@vandelint.net>
2018-04-04 10:58:06 +0000

    three products

--- src/main/java/nl/bart/App.java
+++ src/main/java/nl/bart/App.java
@@ -9,9 +9,12 @@ public class App {
 
     public static void main(String[] args) {
         Supermarket supermarket = new Supermarket();
+        Customer customer = new Customer();
         Discount twoForThree = new Discount(34, "2 for price of 3", "","", 5, 9, true);
+        Discount hotDeal = new Discount(99, "Hot Deal!", "", "", 5, 19, true);
         Product milk = new Product("Milk", 99, twoForThree);
         Product sugar = new Product("Sugar", 1, twoForThree);
+        Product sambal = new Product("Sambal", 199, hotDeal);
 
         supermarket.setBrand("Laidl");
         supermarket.addProduct(milk);
@@ -19,5 +22,8 @@ public class App {
         supermarket.addProduct(sugar);
         supermarket.removeProduct(milk);
 
+        System.out.println();
+
+        customer.addProduct(milk);
     }
 }
